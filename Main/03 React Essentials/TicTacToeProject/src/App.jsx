8215b70320d9caa869b410e6412a07ee872d14aa
MazenThinks <mazenmagdy190.mm@gmail.com>
2025-07-31 18:43:32 +0300

    Players Name Section Done

---
+++ Main/03 React Essentials/TicTacToeProject/src/App.jsx
@@ -0,0 +1,19 @@
+import Player from "./Components/Player";
+
+function App() {
+  return (
+    <>
+      <main>
+        <div id="game-container">
+          <ol id="players">
+            <Player initName="Player 1" symbol="X" />
+            <Player initName="Player 2" symbol="O" />
+          </ol>
+          GAME BOARD
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ Main/03 React Essentials/TicTacToeProject/src/Components/Player.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function Player({ initName, symbol }) {
+  const [isEdited, SetIsEdited] = useState(true);
+  const [playerName, SetplayerName] = useState(initName);
+
+  function handleClick() {
+    SetIsEdited((isEdited) => !isEdited);
+  }
+
+  function handleChange(event) {
+    SetplayerName(event.target.value);
+  }
+
+  return (
+    <li>
+      {isEdited ? (
+        <span className="player">
+          <span className="player-name">{playerName}</span>
+          <span className="player-symbol">{symbol}</span>
+        </span>
+      ) : (
+        <span className="player">
+          <input
+            type="text"
+            required
+            defaultValue={playerName}
+            onChange={handleChange}
+          />
+          <span className="player-symbol">{symbol}</span>
+        </span>
+      )}
+
+      <button onClick={handleClick}>{isEdited ? "Edit" : "Save"}</button>
+    </li>
+  );
+}
